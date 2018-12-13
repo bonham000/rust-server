340ab5a4f39b8f72ce88b62b1b1a178c94994068
Sean Smith <sean.smith@tenx.tech>
2018-12-13 15:18:51 +0800

    Adds TcpListener code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use std::io::prelude::*;
+use std::net::TcpStream;
+use std::net::TcpListener;
+
 fn main() {
-    println!("Hello, world!");
+
+    println!("Initializing...");
+
+    let binding = TcpListener::bind("127.0.0.1:7878");
+
+    match binding {
+        Err(_) => println!("Could not bind to port, is it already in use?"),
+        Ok(listener) => handle_connection(listener),
+    }
+}
+
+fn handle_connection(listener: TcpListener) {
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        println!("Connected!");
+
+        read_stream(stream);
+    }
+}
+
+fn read_stream(mut stream: TcpStream) {
+    let mut buffer = [0; 512];
+
+    stream.read(&mut buffer).unwrap();
+
+    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 }
