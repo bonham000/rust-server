116bd8b19ad628ef1f7b059c902f326bf08a6d11
Sean Smith <sean.smith@tenx.tech>
2018-12-15 21:27:57 +0800

    Handle / and 404 routes

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 use std::io::prelude::*;
+use std::fs;
 use std::net::TcpStream;
 use std::net::TcpListener;
 
 fn main() {
 
-    println!("Initializing...");
+    let port = "127.0.0.1:7878";
 
-    let binding = TcpListener::bind("127.0.0.1:7878");
+    println!("Initializing... running on port: {}", port);
+
+    let binding = TcpListener::bind(port);
 
     match binding {
         Err(_) => println!("Could not bind to port, is it already in use?"),
@@ -20,14 +23,27 @@ fn handle_connection(listener: TcpListener) {
 
         println!("Connected!");
 
-        read_stream(stream);
+        handle_stream(stream);
     }
 }
 
-fn read_stream(mut stream: TcpStream) {
+fn handle_stream(mut stream: TcpStream) {
     let mut buffer = [0; 512];
 
     stream.read(&mut buffer).unwrap();
 
     println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+
+    let get_index_request = b"GET / HTTP/1.1\r\n";
+
+    let (status_line, file_path) = if buffer.starts_with(get_index_request) {
+        ("HTTP/1.1 200 OK\r\n\r\n", "welcome.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
+    };
+
+    let contents = fs::read_to_string(format!("client/{}", file_path)).unwrap();
+    let response = format!("{}{}", status_line, contents);
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
